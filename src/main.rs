86c17b32a1f02849bfa036b7ed1b7586762c06ec
Brian Silah <unpervertedkid@gmail.com>
2023-11-07 09:51:08 +0300

    Pass stage 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader, Write},
+    net::TcpListener,
+};
+
+fn handle_connection(mut stream: std::net::TcpStream) {
+    let buffer = BufReader::new(stream.try_clone().unwrap());
+    let _http_request: Vec<_> = buffer
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);

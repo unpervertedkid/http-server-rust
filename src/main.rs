37631ff6d9709e1a731a2ffe30b32227a4f1ca1c
Brian Silah <unpervertedkid@gmail.com>
2023-11-07 10:29:11 +0300

    Pass stage 5

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::prelude::*;
 use std::net::{TcpStream, TcpListener};
 use std::io::BufReader;
@@ -6,6 +7,7 @@ struct Request {
     _method: String,
     path: String,
     _body: String,
+    headers: HashMap<String, String>,
 }
 
 impl Request {
@@ -19,28 +21,62 @@ impl Request {
     }
 
     fn from_http_request(http_request: Vec<String>) -> Self {
+        let mut headers = HashMap::new();
+        for line in http_request.iter().skip(1) {
+            let parts: Vec<&str> = line.split(": ").collect();
+            if parts.len() == 2 {
+                headers.insert(parts[0].to_string(), parts[1].to_string());
+            }
+        }
+
         Self {
             _method: http_request[0].split_whitespace().nth(0).unwrap().to_string(),
             path: http_request[0].split_whitespace().nth(1).unwrap().to_string(),
             _body: http_request[http_request.len() - 1].to_string(),
+            headers,
         }
     }
 
-    fn handle_request(&self, mut stream: TcpStream) {
-        if self.path == "/" {
-            let response = "HTTP/1.1 200 OK\r\n\r\n";
-            stream.write(response.as_bytes()).unwrap();
-        } else if self.path.starts_with("/echo") {
-            let string = self.path.replace("/echo/", "");
+    fn handle_root(&self, mut stream: TcpStream) {
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+        stream.write(response.as_bytes()).unwrap();
+    }
+
+    fn handle_echo(&self, mut stream: TcpStream) {
+        let string = self.path.replace("/echo/", "");
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            string.len(),
+            string
+        );
+        stream.write(response.as_bytes()).unwrap();
+    }
+
+    fn handle_user_agent(&self, mut stream: TcpStream) {
+        if let Some(user_agent) = self.headers.get("User-Agent") {
             let response = format!(
                 "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                string.len(),
-                string
+                user_agent.len(),
+                user_agent
             );
             stream.write(response.as_bytes()).unwrap();
+        }
+    }
+
+    fn handle_not_found(&self, mut stream: TcpStream) {
+        let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        stream.write(response.as_bytes()).unwrap();
+    }
+
+    fn handle_request(&self, stream: TcpStream) {
+        if self.path == "/" {
+            self.handle_root(stream);
+        } else if self.path.starts_with("/echo") {
+            self.handle_echo(stream);
+        } else if self.path == "/user-agent" {
+            self.handle_user_agent(stream);
         } else {
-            let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-            stream.write(response.as_bytes()).unwrap();
+            self.handle_not_found(stream);
         }
     }
 }

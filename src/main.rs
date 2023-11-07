b1e2f272be28e376329bd717945dbb68533ed4ad
Brian Silah <unpervertedkid@gmail.com>
2023-11-07 10:50:58 +0300

    Pass stage 7

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use std::io::prelude::*;
 use std::io::BufReader;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use std::{env, fs};
 
 struct Request {
     _method: String,
     path: String,
     headers: HashMap<String, String>,
     _body: String,
+    directory: Option<String>,
 }
 
 impl Request {
@@ -21,7 +23,7 @@ impl Request {
             .collect()
     }
 
-    fn from_http_request(http_request: Vec<String>) -> Self {
+    fn from_http_request(http_request: Vec<String>, directory: Option<String>) -> Self {
         let mut headers = HashMap::new();
         for line in http_request.iter().skip(1) {
             let parts: Vec<&str> = line.split(": ").collect();
@@ -43,6 +45,7 @@ impl Request {
                 .to_string(),
             headers,
             _body: http_request[http_request.len() - 1].to_string(),
+            directory,
         }
     }
 
@@ -77,6 +80,31 @@ impl Request {
         stream.write(response.as_bytes()).unwrap();
     }
 
+    fn handle_files(&self, mut stream: TcpStream) {
+        if let Some(directory) = &self.directory {
+            let filename = &self.path[7..]; // remove "/files/" from the path
+            let filepath = format!("{}/{}", directory, filename);
+
+            match fs::read(&filepath) {
+                Ok(contents) => {
+                    let response = format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {}\r\n\r\n{}",
+                        contents.len(),
+                        String::from_utf8_lossy(&contents)
+                    );
+                    stream.write(response.as_bytes()).unwrap();
+                }
+                Err(_) => {
+                    let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+                    stream.write(response.as_bytes()).unwrap();
+                }
+            }
+        } else {
+            let response = "HTTP/1.1 500 INTERNAL SERVER ERROR\r\n\r\n";
+            stream.write(response.as_bytes()).unwrap();
+        }
+    }
+
     fn handle_request(&self, stream: TcpStream) {
         if self.path == "/" {
             self.handle_root(stream);
@@ -84,27 +112,34 @@ impl Request {
             self.handle_echo(stream);
         } else if self.path == "/user-agent" {
             self.handle_user_agent(stream);
-        } else {
+        } else if self.path.starts_with("/files") {
+            self.handle_files(stream);
+        }
+        else {
             self.handle_not_found(stream);
         }
     }
 }
 
-fn handle_connection(stream: TcpStream) {
+fn handle_connection(directory: Option<String>, stream: TcpStream) {
     let http_request = Request::read_request(stream.try_clone().unwrap());
-    let request = Request::from_http_request(http_request);
+    let request = Request::from_http_request(http_request, directory);
     request.handle_request(stream);
 }
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let directory = if args.len() > 2 { Some(args[2].clone()) } else { None };
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
+                let directory = directory.clone();
                 thread::spawn(|| {
-                    handle_connection(stream);
+                    handle_connection(directory, stream);
                 });
             }
             Err(e) => {
